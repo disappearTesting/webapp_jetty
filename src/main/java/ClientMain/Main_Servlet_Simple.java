e397bcb6d5dfda709531ddf9824d83793d473820
Makarov_K <makarov@smartproject.ua>
2018-07-05 13:36:12 +0300

    updated: files from git

--- src/main/java/ClientMain/Main_Servlet_Simple.java
+++ src/main/java/ClientMain/Main_Servlet_Simple.java
@@ -1,8 +1,10 @@
 package ClientMain;
 
 import Connectors.Connector_Simple;
+import Servlets.Servlet_Simple;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main_Servlet_Simple {
 
@@ -13,5 +15,6 @@ public class Main_Servlet_Simple {
         server.addConnector(new Connector_Simple("localhost", 8080).applyConnector());
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new Servlet_Simple()), "/api/servlet");
     }
 }
