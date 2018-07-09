3d270883dea46011dda89ef8ebf666fc30f0080d
Makarov_K <makarov@smartproject.ua>
2018-07-09 13:33:17 +0300

    updated: files

--- src/main/java/ClientMain/Main_Servlet_Simple.java
+++ src/main/java/ClientMain/Main_Servlet_Simple.java
@@ -2,19 +2,32 @@ package ClientMain;
 
 import Connectors.Connector_Simple;
 import Servlets.Servlet_Simple;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main_Servlet_Simple {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.addServlet(new ServletHolder(new Servlet_Simple()), "/api/servlet");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("index.php");
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.setHandlers(new Handler[]{resourceHandler, contextHandler});
 
         Server server = new Server();
+        server.addConnector(new Connector_Simple(new ServerConnector(server),"localhost", 8080).applyConnector());
+        server.setHandler(handlerList);
 
-        server.addConnector(new Connector_Simple("localhost", 8080).applyConnector());
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new Servlet_Simple()), "/api/servlet");
+        server.start();
+        server.join();
     }
 }
